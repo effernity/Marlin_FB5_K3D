69bfe80337e2eee02fbdbf7451569b8c295e42e5
Serj Cash <komanch@gmail.com>
2021-04-08 17:31:44 +0300

    Изменил название принтера на скучное

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Fucking Bear"
+#define CUSTOM_MACHINE_NAME "FBGhost 5"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
